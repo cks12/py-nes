766a1c9afd30514d85cb45da8e0fd9140b6672b5
Caio Caik Fresneda de Souza <caiofresneda@gmail.com>
2023-03-06 02:03:03 -0300

    create class instruction base

--- instruction/__init__.py
+++ instruction/__init__.py
@@ -1,11 +1,12 @@
 from .instruction import *
-from .base import InstructionBase
+from .base import instruction_base
+
 instructionList = [
-    LDAInstruction,
-    SEIInstruction,
-    CLDInstruction
+    lda_instruction,
+    sei_instruction,
+    cld_instruction
 ]
-instruction_mapping: Dict[bytes, InstructionBase] = {}
+instruction_mapping: Dict[bytes, instruction_base] = {}
 
 
 for instruction_class in instructionList:
--- instruction/base.py
+++ instruction/base.py
@@ -1,10 +1,23 @@
-class InstructionBase():
+from CPU.registers import Register
+class instruction_base():
     def __init__(self):
         self.identifier_byte = None
         self.instruction_length = 1
+        self.register = None
+        self.data_bytes = None
 
-    def set(self, byte:bytes):    
+    def set_data_bytes(self, data_bytes):
+        self.data_bytes = data_bytes
+
+    def set_identifier_byte(self, byte:bytes):    
         self.identifier_byte
+
+    def set_register(self, register: Register):
+        self.register = register
+    
+
+    def execute(self):
+        raise NotImplementedError()
 
     def process(self):
         print ("Identifier byte: {}".format(self.identifier_byte.hex()))
