cfdd851daf3b04cbdc206220e4d14948891fd322
Caio Caik Fresneda de Souza <caiofresneda@gmail.com>
2023-03-10 22:22:09 -0300

    Fix bugs

--- CPU/cpu.py
+++ CPU/cpu.py
@@ -6,6 +6,7 @@ from .registers import Register
 
 class CPU:
     def __init__(self):
+
         self.registers = []
         self.rom = None
         self.running = False
@@ -13,37 +14,38 @@ class CPU:
         self.cpu_registers = Register()
 
     def run_rom(self, rom: ROM):
+
         self.rom = rom
         self.cpu_registers.PC = self.rom.header_size
         self.running = True
         self.cpu_registers.start()
+
         while self.running:
-            identifier_byte = self.rom.get_bytes(self.cpu_registers.PC)
-            instruction_class = self.instruction_mapping.get(identifier_byte, None)
-           
-            if instruction_class is not None: 
+            self.process_instructions()
                 
-                # get instruction
-                instruction = instruction_class
-
-                # rom bytes
-                num_data_bytes: int = instruction.instruction_length - 1
-                data_bytes = self.rom.get_bytes(self.cpu_registers.PC + 1, num_data_bytes)
-
-                # instructions sets
-                instruction.set_identifier_byte(identifier_byte)
-                instruction.set_register(self.cpu_registers)
-                instruction.set_data_bytes(data_bytes)
-
-                self.cpu_registers.add_program_counter(instruction.instruction_length)
-                instruction.execute()
-            else:
-                self.cpu_registers.add_program_counter(1)
-                pass
-                # print('not config:')
-
-    def process_instructions(self, instruction: instruction_base):
-        instruction.process()
-    
     def process_instructions(self):
-        pass
+
+        # Get byte in rom and find corresponding instruction
+        identifier_byte = self.rom.get_bytes(self.cpu_registers.PC)
+        instruction_class = self.instruction_mapping.get(identifier_byte, None)
+           
+        if instruction_class is None: 
+            self.cpu_registers.add_program_counter(1)
+            return 
+
+        # get instruction
+        instruction = instruction_class
+
+        # rom bytes
+        num_data_bytes: int = instruction.instruction_length - 1
+        data_bytes = self.rom.get_bytes(self.cpu_registers.PC + 1, num_data_bytes)
+
+        # instructions sets
+
+        instruction.set_data_bytes(data_bytes)
+        instruction.set_identifier_byte(identifier_byte)
+        instruction.set_register(self.cpu_registers)
+        
+        instruction.execute()
+
+        self.cpu_registers.add_program_counter(instruction.instruction_length)
